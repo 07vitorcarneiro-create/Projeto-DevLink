6b9faff9d58c60a2825c9ead8d415a6ab8bc4e36
Vitor <07vitorcarneiro@gmail.com>
2026-05-06 00:47:42 -0300

    css modif

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 function toggleMode(){
   const html = document.documentElement
 
-  if(html.classList.contains("dark")){
-    html.classList.remove("dark")
-    html.classList.add("light")
-  }
-  else{
-    html.classList.add("light")
-  }
+  html.classList.toggle("light")
 
   // pegar a tag img e substituir a foto
 
@@ -17,25 +11,7 @@ function toggleMode(){
     // caso esteja no tema light, vai estar na foto 'light'
     img.setAttribute("src", "./assets/avatar-light.png") // 'setAttribute' = adicionar/ajustar/modificar um atributo.
   }
-}
-
-function modeToggle(){
-  const html = document.documentElement
-
-  if(html.classList.contains("light")){
-    html.classList.remove("light")
-    html.classList.add("dark")
-  }
   else{
-    html.classList.add("dark")
-  }
-
-  // pegar a tag img e substituir a foto
-
-  const img = document.querySelector("#profile img") // 'querySelector' = pesquisa pelo seletor
-
-  if(html.classList.contains("dark")){
-    // caso esteja no tema light, vai estar na foto 'light'
-    img.setAttribute("src", "./assets/avatar.png") // 'setAttribute' = adicionar/ajustar/modificar um atributo.
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
