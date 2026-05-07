a702fce60fa3472a676528c4a30aa066fb50cbc9
Vitor <07vitorcarneiro@gmail.com>
2026-05-06 21:30:27 -0300

    margin on switch

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function toggleMode(){
   const html = document.documentElement
 
-  html.classList.toggle("light")
+  html.classList.toggle("light");
 
   // pegar a tag img e substituir a foto
 
-  const img = document.querySelector("#profile img")// 'querySelector' = pesquisa pelo seletor
+  const img = document.querySelector("#profile img"); // 'querySelector' = pesquisa pelo seletor
 
   if(html.classList.contains("light")){
     // caso esteja no tema light, vai estar na foto 'light'
-    img.setAttribute("src", "./assets/avatar-light.png") // 'setAttribute' = adicionar/ajustar/modificar um atributo.
+    img.setAttribute("src", "./assets/avatar-light.png"); // 'setAttribute' = adicionar/ajustar/modificar um atributo.
   }
   else{
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar.png");
   }
 }
